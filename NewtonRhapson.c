9b2299f233c63e8f86a73afe3cdc3a7e15cc0d94
Narcizo Gabriel <narcizo.gabriel2@gmail.com>
2017-12-06 12:01:37 -0200

    Add files via upload

---
+++ NewtonRhapson.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+
+long double ieee754(float input){
+    int expoente;
+    long double mantissa;
+
+    expoente = floor(log2(input));
+    printf("%d\n", expoente);
+    mantissa = input/pow(2, expoente);
+    printf("%.20Lf\n", mantissa);
+    expoente += 1023; // bias 64bits
+
+    if(fmod(input, 2) != 0)
+        mantissa /= 2;
+
+    return mantissa;
+}
+
+void main(){
+    long double xk, fxk, Dfxk, xk1, erro, precisao;
+    long double input;
+    int i = 0;
+
+    precisao = 0.000000000000000001;
+    scanf(" %Lf", &input);
+    xk1 = ieee754(input);
+    //xk1 = input/4 + 1;
+    do{
+        printf("***********************iteracao = %d\n", i++);
+        xk = xk1;
+        printf("Xk = %.20Lf\n", xk);
+        fxk = (xk * xk) - input;
+        printf("f(Xk) = %.20Lf\n", fxk);
+        Dfxk = 2 * xk;
+        printf("f'(Xk) = %.20Lf\n", Dfxk);
+        xk1 = xk - fxk/Dfxk;
+        printf("Xk+1 = %.20Lf\n", xk1);
+        erro = fabs(xk1 - xk);
+        printf("erro = %.25Lf\n", erro);
+    }while(erro > precisao);
+
+    printf("\n");
+    printf("\nprecisao = %.25Lf\n", precisao);
+    printf("xk = %.30Lf\n", xk);
+}
